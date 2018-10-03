b982583ad51ee2ff955ee59a579604276102b287
Chris <chr1sp1n.dev@gmail.com>
2018-10-03 17:58:38 +0200

    assets fix

--- cau/assets.js
+++ cau/assets.js
@@ -3,6 +3,7 @@
 const gulp = require('gulp');
 const config = require('./config');
 const path = require('path');
+const pathExists = require('path-exists');
 
 // module.exports = {
 
@@ -57,15 +58,27 @@ module.exports = function(done){
 	src.push( path.join( __dirname, config.basePath, config.get('source.path')) + '/**/.*' );
 
 	src.push( '!'+ path.join( __dirname, config.basePath, config.get('source.scss.src')) + '/**' );
-	src.push( '!'+ path.join( __dirname, config.basePath, config.get('source.js.src')) + '/**' );
+	//src.push( '!'+ path.join( __dirname, config.basePath, config.get('source.path')) + '/**' );
+
+	var filesRaw = config.get('source.js.files');
+	filesRaw.forEach( function(f, i) {
+		if(!pathExists.sync(f)){
+			f = path.join( __dirname, config.basePath, config.get('source.js.files['+ i +']') );
+			if(!pathExists.sync(f)) f = false;
+		}
+		if(f) {
+			src.push( '!' + path.join( __dirname, config.basePath,  config.get('source.js.files['+ i +']') ) );
+		}
+	});
 
 	var exceptions = config.get('source.js.inject_to');
 	if(exceptions){
-		exceptions.forEach( function(e) {
-			src.push( '!' + path.join( __dirname, config.basePath, config.get('source.path')) + '/' + e );
+		exceptions.forEach( function(e, i) {
+			src.push( '!' + path.join( __dirname, config.basePath, config.get('source.js.inject_to['+ i +']') ) );
 		});
 	}
 
+	console.log(path.join(__dirname, config.basePath, config.get('temp_path') + '/' ));
 	return gulp.src( src )
 		.pipe( gulp.dest( path.join(__dirname, config.basePath, config.get('temp_path') + '/' )) );
 
